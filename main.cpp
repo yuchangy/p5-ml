fde1b6ebddc5659b91828d8ac467545700b92445
yuchangy <112911545+yuchangy@users.noreply.github.com>
2023-04-15 16:23:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,10 +15,74 @@
 
 using namespace std;
 
+class EECS280{
+private:
+    int numPosts;
+    int uniqueWords;
+    set<string> labels;
+    map<string, int> wordSearch; //This item is used to search for the word (use find function)
+    map<string, int> labelSearch; //This item is used to search through so
+    map<pair<string, string>, int> map_pair;
+    
+public:    
+    int get_numPosts(){
+        return numPosts;
+    }
+    
+    int get_uniqueWords(){
+        return uniqueWords;
+    }
+    
+    map<string, int> get_labelSearch(){
+        return labelSearch;
+    }
+    
+    set<string> get_labels(){
+        return labels;
+    }
+    
+    map<pair<string, string>, int> map_search(map<string, string>){
+        assert(false);
+    }
+    
+    //for(auto i:m ){
+    //  cout << i.first << " " << i.second << endl;
+    
+    void open_file(string data){
+        csvstream csvin(data);
+        map<string, string> row;
+        
+        while(csvin >> row){
+            numPosts++;
+        }
+        
+    }
+    
+    //calculation:
+    double label_probability(){
+        double prediction = 0;
+        return prediction;
+    }
+    double label_probability_with_word(){
+        double prediction = 0;
+        return prediction;
+    }
+    
+    double final_probability(){
+        double prediction = 0;
+        return prediction;
+    }
+    
+};
+
+
 int main(int argc, const char * argv[]) {
     
+    std::cout.precision(3);
+    string input = argv[1];
+    ifstream fin(input);
     //Command Line Check:
-    if(argc != 3 || argc != 4){
+    if(argc != 3 && argc != 4){
         std::cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << std::endl;
         return -1;
     }
@@ -26,42 +90,19 @@ int main(int argc, const char * argv[]) {
     if(std::string(argv[3]) != "--debug"){
         std::cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << std::endl;
         return -1;
-
     }
-    //Construct Classifier ADT:
-    class EECS280{
-    private:
-        int numPosts = 0;
-        int uniqueWords = 0;
-        map<string, int> wordSearch;
-        map<string, int> labeSearch;
-        map<pair<string, string>, int> map_pair;
-        set<string> unique_words;
-        
-    public:
-        //Training:
-        std::string file_name;
-        
-        
-        //calculation:
-        double label_probability(){
-            double prediction = 0;
-            return prediction;
-        }
-        double label_probability_with_word(){
-            double prediction = 0;
-            return prediction;
-        }
-        
-        double final_probability(){
-            double prediction = 0;
-            return prediction;
-        }
-        
-    };
+
+    if (!fin.is_open()) {
+        cout << "Error opening file: " << input << endl;
+        return -1;
+    }
+    
+    EECS280 prediction;
+    prediction.open_file(input);
+    
+    
     
     //Training:
-    
     
     
     //Testing:
