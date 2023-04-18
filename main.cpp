0383a49ec961f2512f4fb7e813396dc39ff0ef49
yuchangy <112911545+yuchangy@users.noreply.github.com>
2023-04-18 14:03:47 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ private:
     map<string, int> wordSearch; //The number of posts with the word string
     map<string, int> labelSearch; //This item is used to search through so
     map<string, map<string, int>> map_pair;
+    map<string, string> all_data;
     
 public:
     //training portion:
@@ -32,7 +33,7 @@ public:
         while(csvin >> row){
             numPosts++;
             if(label_is_in(row["tag"]) == false){
-                labelSearch.insert({row["tag"], 0});
+                labelSearch.insert({row["tag"], 1});
             }
             
             else if(label_is_in(row["tag"])== true){
@@ -40,7 +41,9 @@ public:
             }
             
             unique_words(row["content"], row["tag"]);
+            all_data[row["tag"]] =  row["content"];
         }
+        uniqueWords = wordSearch.size();
     }
     
     set<string> unique_words(const string &str, const string &tag) {
@@ -50,7 +53,6 @@ public:
         
         while (source >> word) {
               words.insert({word});
-            uniqueWords++;
         }
         
         for(auto it = words.begin(); it != words.end(); it++){
@@ -58,6 +60,7 @@ public:
             wordSearch[value]++;
             map_pair[tag][value]++;
         }
+        
       return words;
     }
     
@@ -68,11 +71,74 @@ public:
         }
         return false;
     }
+    
+    void debug(string data){
+        //print trained Data
+        std::cout << "training data: " << "\n";
+        for(auto it = all_data.begin(); it != all_data.end(); it++){
+            std::cout << "  label = " << it->first << ", content = " << it->second << "\n"; 
+        }
+        std::cout << "trained on " << numPosts << " examples" << "\n";
+        std::cout << "vocabulary size = " << uniqueWords << "\n" << "\n";
+        
+        //print test daata
+        std::cout << "classes:" << "\n";
+        for(auto it = labelSearch.begin(); it != labelSearch.end(); it++){
+            std::cout << "  " << it->first << ", "<< it->second << " examples, log-prior = "<< label_probability(it->first) << "\n";
+        }
+        
+        //classifier parameters:
+        std::cout << "classifier parameters: " << "\n";
+        
+        for(auto outer = map_pair.begin(); outer != map_pair.end(); outer++){
+            for(auto inner = outer->second.begin(); inner != outer->second.end();  inner++){
+                std::cout << "  " << outer->first << ":" << inner->first <<", count = " << inner->second << ", log-likelihood = " << label_probability_with_word(inner->first, outer->first) << "\n";
+            }
+        }
+        test_debug(data);
+    }
         
     void train(string data){
         load(data);
     }
     
+    void test_debug(string data){
+        set<string> post;
+        string final_predict_label = "";
+        map<string, double> test_map_item;
+        double log_probability_score = 0;
+        int performance = 0;
+        int num_test_post = 0;
+        
+        
+        csvstream csvin(data);
+        map<string, string> row;
+        std::cout << "test data:" << "\n";
+        
+        while(csvin >> row){
+            post = test_words(row["content"]);
+            
+            test_map_item = test_map(row["tag"], post);
+            
+            final_predict_label = final_probability_answer(test_map_item); //this returns label string
+            
+            log_probability_score = final_probability_num(test_map_item,
+                                                          final_predict_label);
+            // Return the log prob score associated with the label
+            
+            std::cout << "  correct = " << row["tag"] <<
+                        " predicted = " << final_predict_label <<
+            " log-probability score = " << log_probability_score <<"\n";
+            
+            std::cout << "  content = " << row["content"] << "\n";
+            
+            if(row["tag"] == final_predict_label){
+                performance++;
+            }
+            num_test_post++;
+        }
+        std::cout << "performance: " << performance << " / " << num_test_post << " posts predicted correctly" << "\n";
+    }
     
     void test(string data){
         set<string> post;
@@ -93,14 +159,17 @@ public:
             
             test_map_item = test_map(row["tag"], post);
             
-            final_predict_label = final_probability_answer(test_map_item);
+            final_predict_label = final_probability_answer(test_map_item); //this returns label string
             
-            log_probability_score = final_probability_num(test_map_item, final_predict_label);
+            log_probability_score = final_probability_num(test_map_item,
+                                                          final_predict_label);
+            // Return the log prob score associated with the label
             
-            std::cout << "  correct = " << row["tag"] << " predicted = " << final_predict_label <<
+            std::cout << "  correct = " << row["tag"] <<
+                        " predicted = " << final_predict_label <<
             " log-probability score = " << log_probability_score <<"\n";
             
-            std::cout << "content = " << row["content"] << "\n";
+            std::cout << "  content = " << row["content"] << "\n";
             
             if(row["tag"] == final_predict_label){
                 performance++;
@@ -188,7 +257,8 @@ public:
         
        double label_probability_with_word(string str, string tag){
            double prediction = 0;
-           double divide = (find_label_word_numPost(str, tag)/find_label_numPost(str));
+           double divide = 0;
+           divide = (find_label_word_numPost(str, tag)/find_label_numPost(tag));
            prediction = log(divide);
            return prediction;
        }
@@ -233,20 +303,28 @@ public:
 int main(int argc, const char * argv[]) {
     
     std::cout.precision(3);
-    string input = argv[1];
-    string input_test = argv[2];
+    string input = "/Users/andrewyu/Desktop/280/p5-ml/train_small.csv";
+    string input_test = "/Users/andrewyu/Desktop/280/p5-ml/test_small.csv";
     ifstream fin(input);
     ifstream testfin(input_test);
-    //Command Line Check:
-    if(argc != 3 && argc != 4){
-        std::cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << std::endl;
-        return -1;
-    }
+    EECS280 prediction;
 
-    if(std::string(argv[3]) != "--debug"){
-        std::cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << std::endl;
-        return -1;
-    }
+    //Command Line Check:
+//    if(argc != 3 && argc != 4){
+//        std::cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << std::endl;
+//        return -1;
+//    }
+    
+//    if(std::string(argv[3]) == "--debug"){
+//          prediction.train(input);
+//          prediction.debug(input_test);
+//    }
+//
+//    else if(std::string(argv[3]) != "--debug"){
+//        std::cout << "Usage: main.exe TRAIN_FILE TEST_FILE [--debug]" << std::endl;
+//
+//        return -1;
+//    }
 
     if (!fin.is_open()) {
         cout << "Error opening file: " << input << endl;
@@ -258,9 +336,8 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
     
-    EECS280 prediction;
     prediction.train(input);
-    prediction.test(input_test);
+    prediction.debug(input_test);
     
 }
 
